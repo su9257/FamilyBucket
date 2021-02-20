592c5a5757d6987cc47bc02e6973b06286c8dc1e
Administrator <su9257@outlook.com>
2021-02-20 16:17:11 +0800

    PureMVC注释更新

--- PureMVC_Standard/Assets/Framework/PureMVC/Patterns/Facade/Facade.cs
+++ PureMVC_Standard/Assets/Framework/PureMVC/Patterns/Facade/Facade.cs
@@ -13,38 +13,17 @@ using PureMVC.Patterns.Observer;
 namespace PureMVC.Patterns.Facade
 {
     /// <summary>
-    /// A base Singleton <c>IFacade</c> implementation.
+    /// Facade 实例 负责 Model、View、Controller 三个核心类对外接口的调用
     /// </summary>
-    /// <remarks>
-    ///     <para>
-    ///         In PureMVC, the <c>Facade</c> class assumes these 
-    ///         responsibilities:
-    ///     </para>
-    ///     <list type="bullet">
-    ///         <item>Initializing the <c>Model</c>, <c>View</c> and <c>Controller</c> Singletons.</item>
-    ///         <item>Providing all the methods defined by the <c>IModel, IView, and IController</c> interfaces.</item>
-    ///         <item>Providing the ability to override the specific <c>Model</c>, <c>View</c> and <c>Controller</c> Singletons created.</item>
-    ///         <item>Providing a single point of contact to the application for registering <c>Commands</c> and notifying <c>Observers</c></item>
-    ///     </list>
-    /// </remarks>
     /// <seealso cref="PureMVC.Core.Model"/>
     /// <seealso cref="PureMVC.Core.View"/>
     /// <seealso cref="PureMVC.Core.Controller"/>
     public class Facade : IFacade
     {
         /// <summary>
-        /// Constructor.
+        /// 构造函数，会以M、C、V的顺序初始化对应的核心类
         /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         This <c>IFacade</c> implementation is a Singleton, 
-        ///         so you should not call the constructor 
-        ///         directly, but instead call the static Factory method, 
-        ///         passing the unique key for this instance 
-        ///         <c>Facade.getInstance(() => new Facade())</c>
-        ///     </para>
-        /// </remarks>
-        /// <exception cref="System.Exception">Thrown if instance for this Singleton key has already been constructed</exception>
+        /// <exception cref="System.Exception">如果重复构造了这个单例类，则抛出异常</exception>
         public Facade()
         {
             if (instance != null) throw new Exception(SingletonMsg);
@@ -53,15 +32,8 @@ namespace PureMVC.Patterns.Facade
         }
 
         /// <summary>
-        /// Initialize the Singleton <c>Facade</c> instance.
+        /// 会以M、C、V的顺序初始化对应的核心类
         /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         Called automatically by the constructor. Override in your
-        ///         subclass to do any subclass specific initializations. Be
-        ///         sure to call <c>super.initializeFacade()</c>, though.
-        ///     </para>
-        /// </remarks>
         protected virtual void InitializeFacade()
         {
             InitializeModel();
@@ -70,10 +42,10 @@ namespace PureMVC.Patterns.Facade
         }
 
         /// <summary>
-        /// Facade Singleton Factory method
+        /// 获取Faced单例类
         /// </summary>
-        /// <param name="facadeFunc">the <c>FuncDelegate</c> of the <c>IFacade</c></param>
-        /// <returns>the Singleton instance of the Facade</returns>
+        /// <param name="facadeFunc">实例化 IFacade 的Func委托</param>
+        /// <returns>返回IFacade实例</returns>
         public static IFacade GetInstance(Func<IFacade> facadeFunc)
         {
             if (instance == null)
@@ -84,200 +56,142 @@ namespace PureMVC.Patterns.Facade
         }
 
         /// <summary>
-        /// Initialize the <c>Controller</c>.
+        /// 初始化并获取 Controller 核心类引用
         /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         Called by the <c>initializeFacade</c> method.
-        ///         Override this method in your subclass of <c>Facade</c> 
-        ///         if one or both of the following are true:
-        ///         <list type="bullet">
-        ///             <item>You wish to initialize a different <c>IController</c>.</item>
-        ///             <item>You have <c>Commands</c> to register with the <c>Controller</c> at startup.</item>
-        ///         </list>
-        ///     </para>
-        ///     <para>
-        ///         If you don't want to initialize a different <c>IController</c>, 
-        ///         call <c>super.initializeController()</c> at the beginning of your
-        ///         method, then register <c>Command</c>s.
-        ///     </para>
-        /// </remarks>
         protected virtual void InitializeController()
         {
             controller = Controller.GetInstance(() => new Controller());
         }
 
         /// <summary>
-        /// Initialize the <c>Model</c>.
+        /// 初始化并获取 Model 核心类引用
         /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         Called by the <c>initializeFacade</c> method.
-        ///         Override this method in your subclass of <c>Facade</c> 
-        ///         if one or both of the following are true:
-        ///         <list type="bullet">
-        ///             <item>You wish to initialize a different <c>IModel</c>.</item>
-        ///             <item>You have <c>Proxy</c>s to register with the <c>Model</c> that do not
-        ///                 retrieve a reference to the Facade at construction time.
-        ///             </item>
-        ///         </list>
-        ///     </para>
-        ///     <para>
-        ///         If you don't want to initialize a different <c>IModel</c>, 
-        ///         call <c>super.initializeModel()</c> at the beginning of your
-        ///         method, then register <c>Proxy</c>s.
-        ///     </para>
-        ///     <para>
-        ///         Note: This method is <i>rarely</i> overridden; in practice you are more
-        ///         likely to use a <c>Command</c> to create and register <c>Proxy</c>s
-        ///         with the <c>Model</c>, since <c>Proxy</c>s with mutable data will likely
-        ///         need to send <c>INotification</c>s and thus will likely want to fetch a reference to 
-        ///         the <c>Facade</c> during their construction. 
-        ///     </para>
-        /// </remarks>
         protected virtual void InitializeModel()
         {
             model = Model.GetInstance(() => new Model());
         }
 
         /// <summary>
-        /// Initialize the <c>View</c>.
+        /// 初始化并获取 View 核心类引用
         /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         Called by the <c>initializeFacade</c> method.
-        ///         Override this method in your subclass of <c>Facade</c> 
-        ///         if one or both of the following are true:
-        ///         <list type="bullet">
-        ///             <item>You wish to initialize a different <c>IView</c>.</item>
-        ///             <item>You have <c>Observers</c> to register with the <c>View</c></item>
-        ///         </list>
-        ///     </para>
-        /// </remarks>
         protected virtual void InitializeView()
         {
             view = View.GetInstance(() => new View());
         }
 
         /// <summary>
-        /// Register an <c>ICommand</c> with the <c>Controller</c> by Notification name.
+        /// 注册 对应消息的 Command 
         /// </summary>
-        /// <param name="notificationName">the name of the <c>INotification</c> to associate the <c>ICommand</c> with</param>
-        /// <param name="factory">a reference to the Class of the <c>ICommand</c></param>
+        /// <param name="notificationName">消息名称</param>
+        /// <param name="factory">构造 Command 的Fun委托</param>
         public virtual void RegisterCommand(string notificationName, Func<ICommand> factory)
         {
             controller.RegisterCommand(notificationName, factory);
         }
 
         /// <summary>
-        /// Remove a previously registered <c>ICommand</c> to <c>INotification</c> mapping from the Controller.
+        /// 主要对应的消息的Command
         /// </summary>
-        /// <param name="notificationName">the name of the <c>INotification</c> to remove the <c>ICommand</c> mapping for</param>
+        /// <param name="notificationName">消息名称</param>
         public virtual void RemoveCommand(string notificationName)
         {
             controller.RemoveCommand(notificationName);
         }
 
         /// <summary>
-        /// Check if a Command is registered for a given Notification 
+        /// 检测对应的消息时候含有对应的Command
         /// </summary>
-        /// <param name="notificationName"></param>
-        /// <returns>whether a Command is currently registered for the given <c>notificationName</c>.</returns>
+        /// <param name="notificationName">消息名称</param>
+        /// <returns>返回是否含有Command的bool值</returns>
         public virtual bool HasCommand(string notificationName)
         {
             return controller.HasCommand(notificationName);
         }
 
         /// <summary>
-        /// Register an <c>IProxy</c> with the <c>Model</c> by name.
+        /// 注册对应的 Proxy
         /// </summary>
-        /// <param name="proxy">the <c>IProxy</c> instance to be registered with the <c>Model</c>.</param>
+        /// <param name="proxy">IProxy实例</param>
         public virtual void RegisterProxy(IProxy proxy)
         {
             model.RegisterProxy(proxy);
         }
 
         /// <summary>
-        /// Retrieve an <c>IProxy</c> from the <c>Model</c> by name.
+        /// 检索对应Proxy
         /// </summary>
-        /// <param name="proxyName">the name of the proxy to be retrieved.</param>
-        /// <returns>the <c>IProxy</c> instance previously registered with the given <c>proxyName</c>.</returns>
+        /// <param name="proxyName">Proxy名称</param>
+        /// <returns>返回Proxy实例</returns>
         public virtual IProxy RetrieveProxy(string proxyName)
         {
             return model.RetrieveProxy(proxyName);
         }
 
         /// <summary>
-        /// Remove an <c>IProxy</c> from the <c>Model</c> by name.
+        /// 注销对应的Proxy
         /// </summary>
-        /// <param name="proxyName">the <c>IProxy</c> to remove from the <c>Model</c>.</param>
-        /// <returns>the <c>IProxy</c> that was removed from the <c>Model</c></returns>
+        /// <param name="proxyName">Proxy名称</param>
+        /// <returns>返回Proxy实例</returns>
         public virtual IProxy RemoveProxy(string proxyName)
         {
             return model.RemoveProxy(proxyName);
         }
 
         /// <summary>
-        /// Check if a Proxy is registered
+        /// 检查是否含有对应的Proxy
         /// </summary>
-        /// <param name="proxyName"></param>
-        /// <returns>whether a Proxy is currently registered with the given <c>proxyName</c>.</returns>
+        /// <param name="proxyName">Proxy名称</param>
+        /// <returns>返回是否含有bool值</returns>
         public virtual bool HasProxy(string proxyName)
         {
             return model.HasProxy(proxyName);
         }
 
         /// <summary>
-        /// Register a <c>IMediator</c> with the <c>View</c>.
+        /// 注册对应的Mediator
         /// </summary>
-        /// <param name="mediator">a reference to the <c>IMediator</c></param>
+        /// <param name="mediator">Mediator名称</param>
         public virtual void RegisterMediator(IMediator mediator)
         {
             view.RegisterMediator(mediator);
         }
 
         /// <summary>
-        /// Retrieve an <c>IMediator</c> from the <c>View</c>.
+        /// 检索对应的Mediator
         /// </summary>
-        /// <param name="mediatorName"></param>
-        /// <returns>the <c>IMediator</c> previously registered with the given <c>mediatorName</c>.</returns>
+        /// <param name="mediatorName">Mediator名称</param>
+        /// <returns>返回Mediator实例</returns>
         public virtual IMediator RetrieveMediator(string mediatorName)
         {
             return view.RetrieveMediator(mediatorName);
         }
 
         /// <summary>
-        /// Remove an <c>IMediator</c> from the <c>View</c>.
+        /// 移除对应的Mediator
         /// </summary>
-        /// <param name="mediatorName">name of the <c>IMediator</c> to be removed.</param>
-        /// <returns>the <c>IMediator</c> that was removed from the <c>View</c></returns>
+        /// <param name="mediatorName">Mediator名称</param>
+        /// <returns>返回Mediator实例</returns>
         public virtual IMediator RemoveMediator(string mediatorName)
         {
             return view.RemoveMediator(mediatorName);
         }
 
         /// <summary>
-        /// Check if a Mediator is registered or not
+        /// 检查是否含有对应的Mediator
         /// </summary>
-        /// <param name="mediatorName"></param>
-        /// <returns>whether a Mediator is registered with the given <c>mediatorName</c>.</returns>
+        /// <param name="Mediator名称"></param>
+        /// <returns>返回是否含有Mediator的bool值</returns>
         public virtual bool HasMediator(string mediatorName)
         {
             return view.HasMediator(mediatorName);
         }
 
         /// <summary>
-        /// Create and send an <c>INotification</c>.
+        /// 创一个Notification并发送
         /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         Keeps us from having to construct new notification 
-        ///         instances in our implementation code.
-        ///     </para>
-        /// </remarks>
-        /// <param name="notificationName">the name of the notiification to send</param>
-        /// <param name="body">the body of the notification (optional)</param>
-        /// <param name="type">type the type of the notification (optional)</param>
+        /// <param name="notificationName">notification名称</param>
+        /// <param name="body">传输的数据结构</param>
+        /// <param name="type">用于区分对应的Notification的类别</param>
         public virtual void SendNotification(string notificationName, object body = null, string type = null)
         {
             NotifyObservers(new Notification(notificationName, body, type));
@@ -292,17 +206,17 @@ namespace PureMVC.Patterns.Facade
             view.NotifyObservers(notification);
         }
 
-        /// <summary>References to Controller</summary>
+        /// <summary>Controller 引用</summary>
         protected IController controller;
-        /// <summary>Reference to Model</summary>
+        /// <summary>Model 引用</summary>
         protected IModel model;
-        /// <summary>References to View</summary>
+        /// <summary>View 引用</summary>
         protected IView view;
 
-        /// <summary>Singleton instance</summary>
+        /// <summary>单例实例</summary>
         protected static IFacade instance;
 
-        /// <summary>Message Constants</summary>
+        /// <summary>报错信息</summary>
         protected const string SingletonMsg = "Facade Singleton already constructed!";
     }
 }
