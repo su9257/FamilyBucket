cd1b72b95b5e652c796be1c515476823c2fd33c3
su9257 <su9257@outlook.com>
2021-03-05 13:49:47 +0800

    工程初始化

---
+++ Demo/Assets/Plugins/Framework/Extension/IMonoBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public interface IMonoBehaviour 
+{
+    void Awake();
+    void Start();
+    void Update();
+    void OnDestroy();
+}
